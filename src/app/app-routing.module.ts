f6787d258d65696a77234c2859ae94cb3c7979b9
Camille Payen <camille.payen.pro@gmail.com>
2020-08-02 19:40:41 +0200

    Add main routes and guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,31 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { NotesComponent } from './components/notes/notes.component';
+import { EntriesComponent } from './components/entries/entries.component';
+import { LoginComponent } from './components/login/login.component';
+import { AuthGuard } from './helpers';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { 
+    path: '', 
+    component: NotesComponent, 
+    canActivate: [AuthGuard], 
+    children: [
+      { 
+        path: '', 
+        component: EntriesComponent 
+      },
+      { 
+        path: 'list/:path', 
+        component: EntriesComponent 
+      }
+    ]
+  },
+  { 
+    path: 'login', 
+    component: LoginComponent 
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NotesComponent } from './components/notes/notes.component';
+import { TreeComponent } from './components/tree/tree.component';
+import { EntriesComponent } from './components/entries/entries.component';
+import { LoginComponent } from './components/login/login.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NotesComponent,
+    TreeComponent,
+    EntriesComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
